478917734959d5c7033461b78e48ffa0876a88c9
ScrutchWasTaken <estelle.moncollin@gmail.com>
2015-01-14 18:04:14 +0100

    Protocole: mise en place du port 5552

--- fr.ensisa.hassenforder.game.empty.common/src/fr/ensisa/hassenforder/network/Protocol.java
+++ fr.ensisa.hassenforder.game.empty.common/src/fr/ensisa/hassenforder/network/Protocol.java
@@ -2,7 +2,7 @@ package fr.ensisa.hassenforder.network;
 
 public interface Protocol {
 
-	static final public int GAME_PORT_ID = 0;
+	static final public int GAME_PORT_ID = 5552;
 
 	public static final Object EXIT_TEXT = "exit";
 
